8f0e5d73dc853f21151ba453c3ae62616e0e4cc9
Manoela Mirtes <manoela.mirtes@gmail.com>
2018-02-27 20:56:01 -0300

    Classe de execessoes

---
+++ Excessoes.java
@@ -0,0 +1,19 @@
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class Excessoes {
+	
+	
+	public static void adicionarMensagemSucesso(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+	}
+	
+	public static void adicionarMensagemErro(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+	}
+
+}
