d4857dcb2339353cc0bf4a743898867866551beb
Manoela Mirtes <manoela.mirtes@gmail.com>
2018-02-27 21:26:20 -0300

    Criação da classe usuario para o login

---
+++ src/br/com/studentroom/domain/Usuario.java
@@ -0,0 +1,12 @@
+package br.com.studentroom.domain;
+import br.com.studentroom.domain.Empresa;
+public class Usuario {
+	private Empresa idEmpresa;
+	private String senha;
+	
+	public Usuario () {
+		super();
+	}
+	
+
+}
