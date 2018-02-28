4d7a177b798bba8fd0e973fcdc48359924f7ce27
Manoela Mirtes <manoela.mirtes@gmail.com>
2018-02-28 15:09:59 -0300

    Criacao da classe Salaview

--- src/br/com/studentroom/view/ClienteView.java
+++ src/br/com/studentroom/view/ClienteView.java
@@ -1,17 +1,11 @@
 package br.com.studentroom.view;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
 import br.com.studentroom.domain.Cliente;
 import br.com.studentroom.DAO.ClienteDAO;
-import br.com.studentroom.factory.ConexaoBD;
 
-@SuppressWarnings("unused")
+
+
 public class ClienteView {
 	
 	
--- src/br/com/studentroom/view/SalaView.java
+++ src/br/com/studentroom/view/SalaView.java
@@ -1,5 +1,30 @@
 package br.com.studentroom.view;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.studentroom.DAO.SalaDAO;
+import br.com.studentroom.domain.Sala;
+
 
 public class SalaView {
+	
+	private Sala sala;
+	
+	private ArrayList<Sala>itens;
+	private List<Sala>itenProcurado;
+	
+	public Sala getSala() {
+		return sala;
+	}
 
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	
+	
+
+	
+	
+	
+	
 }
